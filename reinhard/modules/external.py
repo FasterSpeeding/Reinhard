9500399e6c6b53c3a13e39e9898821d4384d9f3a
Faster Speeding <luke@lmbyrne.dev>
2020-06-10 18:37:00 +0100

    Add basis for using nekos.life/api/v2

--- reinhard/modules/external.py
+++ reinhard/modules/external.py
@@ -14,10 +14,6 @@ from ..util import command_hooks
 from ..util import constants
 from ..util import paginators
 
-if typing.TYPE_CHECKING:
-    from hikari import applications
-    from hikari import users
-
 
 exports = ["UtilCluster"]
 
@@ -38,9 +34,8 @@ class UtilCluster(clusters.Cluster):
                 on_error=command_hooks.error_hook, on_conversion_error=command_hooks.on_conversion_error
             ),
         )
-        self.application: typing.Optional[applications.Application] = None
-        self.user: typing.Optional[users.MyUser] = None
         self.paginator_pool = paginators.PaginatorPool(self.components)
+        self.user_agent: str = ""
 
     def filter_error(self, content: str) -> str:
         content.replace(self.components.config.token, "<REDACTED>")
@@ -49,20 +44,23 @@ class UtilCluster(clusters.Cluster):
         return content
 
     async def load(self) -> None:
-        self.application = await self.components.rest.fetch_my_application_info()
-        self.user = await self.components.rest.fetch_me()
+        application = await self.components.rest.fetch_my_application_info()
+        owner_id = application.team.owner_user_id if application.team else application.owner.id
+        me = await self.components.rest.fetch_me()
+        self.user_agent = f"Reinhard discord bot (id:{me.id}; owner:{owner_id})"
         await super().load()
 
     @decorators.command(greedy="query")
     async def lyrics(self, ctx: commands.Context, query: str) -> None:
-        owner_id = self.application.team.owner_user_id if self.application.team else self.application.owner.id
         async with aiohttp.ClientSession(
-            headers={"User-Agent": f"Reinhard (id:{self.user.id}; owner:{owner_id})"}
+            headers={"User-Agent": self.user_agent}
         ) as session:
             response = await session.get("https://lyrics.tsu.sh/v1", params={"q": query})
+
             if response.status == 404:
                 await ctx.message.safe_reply(content=f"Couldn't find the lyrics for `{query}`")
                 return  # TODO: handle all 4XX
+
             if response.status >= 500:
                 await ctx.message.safe_reply(content=f"Failed to fetch lyrics due to server error {response.status}")
                 self.logger.exception(
@@ -121,7 +119,8 @@ class UtilCluster(clusters.Cluster):
         resource_type = resource_type.lower()
         if resource_type not in ("channel", "playlist", "video"):
             await ctx.message.reply(content="Resource type must be one of 'channel', 'playist' or 'video'.")
-        owner_id = self.application.team.owner_user_id if self.application.team else self.application.owner.id
+            return
+
         parameters = {
             "key": ctx.components.config.tokens.google,
             "maxResults": 50,
@@ -131,8 +130,10 @@ class UtilCluster(clusters.Cluster):
             "safeSearch": "none" if False else "strict",  # TODO: channel.nsfw
             "type": resource_type,
         }
+
         if region is not None:
             parameters["regionCode"] = region
+
         if language is not None:
             parameters["relevanceLanguage"] = language
 
@@ -141,16 +142,18 @@ class UtilCluster(clusters.Cluster):
         ) -> typing.AsyncIterator[typing.Tuple[str, embeds.Embed]]:
             next_page_token = ""
             async with aiohttp.ClientSession(
-                headers={"User-Agent": f"Reinhard (id:{self.user.id}; owner:{owner_id})"}
+                headers={"User-Agent": self.user_agent}
             ) as session:
                 while response := await session.get(
                     "https://www.googleapis.com/youtube/v3/search", params={"pageToken": next_page_token, **parameters_}
                 ):
                     if response.status == 404:
                         raise errors.CommandError(f"Couldn't find `{query}`.")
+
                     if response.status >= 500 and next_page_token == "":
                         await self.log_bad_youtube_response(response)
                         raise errors.CommandError("Failed to reach youtube at this time, please try again later.")
+
                     if response.status >= 400 and next_page_token == "":
                         try:
                             error = (await response.json())["error"]["message"]
@@ -178,11 +181,11 @@ class UtilCluster(clusters.Cluster):
                             raise errors.CommandError("Youtube returned invalid data.")
                         raise exc
 
-                    next_page_token = data.get("nextPageToken")
                     for page in data["items"]:
                         response_type = YOUTUBE_TYPES.get(page["id"]["kind"])
                         yield f"{response_type[1]}{page['id'][response_type[0]]}", ...
-                    if next_page_token is None:
+
+                    if (next_page_token := data.get("nextPageToken")) is None:
                         break
 
         paginator = get_next_page(parameters)
@@ -196,19 +199,18 @@ class UtilCluster(clusters.Cluster):
             )
             break
         else:
-            #  data["pageInfo"]["totalResults"] will not reliably be `0` when no data is returned and they don't use 404
-            #  for that so we'll just check to see if nothing is being returned.
+            # data["pageInfo"]["totalResults"] will not reliably be `0` when no data is returned and they don't use 404
+            # for that so we'll just check to see if nothing is being returned.
             await ctx.message.safe_reply(content=f"Couldn't find `{query}`.")
 
     @decorators.command  # TODO: https://lewd.bowsette.pictures/api/request
     async def moe(self, ctx: commands.Context, source: typing.Optional[str] = None) -> None:
-        owner_id = self.application.team.owner_user_id if self.application.team else self.application.owner.id
         params = {}
         if source is not None:
             params["source"] = source
 
         async with aiohttp.ClientSession(
-            headers={"User-Agent": f"Reinhard (id:{self.user.id}; owner:{owner_id})"}
+            headers={"User-Agent": self.user_agent}
         ) as session:
             response = await session.get("http://api.cutegirls.moe/json", params=params)
             if response.status == 404:
@@ -240,3 +242,39 @@ class UtilCluster(clusters.Cluster):
                 raise exc
             else:
                 await ctx.message.reply(content=f"{data['image']} (source {data.get('source') or 'unknown'})")
+
+    async def query_nekos_life(self, endpoint: str, response_key: str, **kwargs: typing.Any) -> str:
+        async with aiohttp.ClientSession(
+            headers={"User-Agent": self.user_agent}
+        ) as session:
+            response = await session.get(url := "https://nekos.life/api/v2" + endpoint)
+            try:
+                data = await response.json()
+            except ValueError:
+                data = None
+
+            # Ok so here's a fun fact, whoever designed this api seems to have decided that it'd be appropriate to
+            # return error status codes in the json body under the "msg" key while leaving the response as a 200 OK
+            # (e.g. a 200 with the json payload '{"msg": "404"}') so here we have to try to get the response code from
+            # the json payload (if available) and then fall back to the actual status code.
+            # We cannot consistently rely on this behaviour either as any internal server errors will likely return an
+            # actual 5xx response.
+            try:
+                status_code = int(data["msg"])
+            except (KeyError, ValueError, TypeError):
+                status_code = response.status
+
+            if status_code == 404:
+                raise errors.CommandError("Query not found.")
+
+            if status_code >= 500 or data is None or response_key not in data:
+                raise errors.CommandError(
+                    "Unable to fetch image at the moment due to server error or malformed response."
+                )
+
+            if status_code >= 300:
+                raise errors.CommandError(
+                    f"Unable to fetch image due to unexpected error {data.get('msg', '')}"
+                )
+
+            return data[response_key]
