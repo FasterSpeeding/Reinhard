8c8b834e2673718cffb818e8c1eb77cb0aed38a3
Faster Speeding <luke@lmbyrne.dev>
2022-01-07 15:39:59 +0000

    Update license

--- reinhard/sql/schema.sql
+++ reinhard/sql/schema.sql
@@ -1,6 +1,6 @@
 -- BSD 3-Clause License
 --
--- Copyright (c) 2021, Lucina
+-- Copyright (c) 2020-2022, Lucina
 -- All rights reserved.
 --
 -- Redistribution and use in source and binary forms, with or without
