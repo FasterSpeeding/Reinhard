ea4fdbd86cb638201bc0c7fc273afbb07a9e653f
Faster Speeding <luke@lmbyrne.dev>
2023-05-02 14:39:41 +0100

    Convert html to markdown instead

--- reinhard/components/docs.py
+++ reinhard/components/docs.py
@@ -37,7 +37,7 @@ import datetime
 import hashlib
 import json
 import typing
-from ..vendored import strip_tags
+import markdownify  # pyright: ignore[reportMissingTypeStubs]
 from collections import abc as collections
 from typing import Annotated
 
@@ -85,8 +85,8 @@ class DocEntry:
         assert isinstance(text, str)
         assert isinstance(title, str)
         self.hashed_location = hash_path(location)
-        self.text = strip_tags.strip_tags(text)
-        self.title = title
+        self.text: str = markdownify.markdownify(text)  # pyright: ignore[reportUnknownMemberType]
+        self.title: str = markdownify.markdownify(title) # pyright: ignore[reportUnknownMemberType]
         self.url = f"{base_url}/{location}"
 
     def to_embed(self) -> hikari.Embed:
